11779952502d7e5f24d27fedb711034318173827
RWThompson <rhystg@yahoo.com>
2017-08-23 14:02:23 +0100

    finished garage html and js files - all working

--- js/garage.js
+++ js/garage.js
@@ -1,44 +1,49 @@
-let car = {
-    regNo: "",
-    make: "",
-    model: "",
-    colour: "",
-    year: 0,
-    numFaults: 0
-};
 let cList = [];
 
 function checkCarIn() {
-    car = (document.getElementById('regNoIn').value, document.getElementById('makeIn').value, document.getElementById('modIn').value, document.getElementById('colIn').value, document.getElementById('yrIn').value, document.getElementById('faultIn').value);
-    let index = cList.length - 1;
+    let car = {
+        regNo: document.getElementById('regNoIn').value,
+        make: document.getElementById('makeIn').value,
+        model: document.getElementById('modIn').value,
+        colour: document.getElementById('colIn').value,
+        year: document.getElementById('yrIn').value,
+        numFaults: document.getElementById('faultIn').value
+    };
+    let index = cList.length;
     cList[index] = car;
-    console.log(cList[0].regNo);
 }
 
 function checkCarOut() {
     for (let i = 0; i < cList.length; i++) {
-        if (cList[i].regNo === document.getElementById('regNoOut')) {
+        if (cList[i].regNo === document.getElementById('regNoOut').value) {
             cList.splice(cList[i], 1);
         }
     }
 }
 
 function outputGarage() {
+    let d = document.getElementById('out');
     for (let i = 0; i < cList.length; i++) {
         let output = document.createElement("h3");
-        output.textContent = (`${cList[i].regNo}, ${cList[i].make}, ${cList[i].model}, ${cList[i].colour}, ${cList[i].year}, ${cList[i].numFaults}`)
-        let d = document.getElementById('out');
+        output.textContent = (`${i + 1}) REG: ${cList[i].regNo}, MAKE: ${cList[i].make}, MODEL: ${cList[i].model}, COLOUR: ${cList[i].colour}, YEAR: ${cList[i].year}, NUMBER OF FAULTS: ${cList[i].numFaults}`);
         d.appendChild(output);
     }
+    let barrier = document.createElement("h3");
+    barrier.textContent = "========================================";
+    d.appendChild(barrier);
 }
 
 function calcBill() {
+    let total = 0;
+    let r = "[REG NO]";
     for (let i = 0; i < cList.length; i++) {
-        if (document.getElementById('regNoBill') === cList[i].regNo) {
-            let total = ((cList[i].year / 100) + (cList[i].numFaults * 20));
-            let billOut = document.createElement("h3");
-            let d = document.getElementById('bill');
-            d.appendChild(billOut);
+        if (cList[i].regNo === document.getElementById('regNoBill').value) {
+            total = ((cList[i].year) / 100 + (cList[i].numFaults) * 20);
+            r = cList[i].regNo;
         }
     }
+    let billOut = document.createElement("h3");
+    let d = document.getElementById('bill');
+    billOut.textContent = (`TOTAL PRICE FOR ${r}: ${total}`);
+    d.appendChild(billOut);
 }
