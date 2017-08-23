e94ce7d206a06704c7f92a3ca6977a1b479a9332
RWThompson <rhystg@yahoo.com>
2017-08-23 20:49:30 +0100

    updated knight.js, working for some destinations, others it causes and infinite loop - DO NOT RUN

--- js/knight.js
+++ js/knight.js
@@ -3,7 +3,7 @@ pointMaker = function (x, y) {
     pt.x = x;
     pt.y = y;
     return pt;
-}
+};
 
 // const v1 = pointMaker(1, 2);
 // const v2 = pointMaker(1, -2);
@@ -21,6 +21,7 @@ function tour() {
     let target = pointMaker(Number(document.getElementById('x').value), Number(document.getElementById('y').value));
     let count = 0;
     let tmp = target;
+    let i = 1;
     console.log(JSON.stringify(tmp));
     do {
         if (tmp.x >= 2 && tmp.y >= 1 && tmp.x > tmp.y) { // BOTH POS
@@ -28,49 +29,106 @@ function tour() {
             tmp.y -= 1;
             count++;
             console.log("1st " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x >= 1 && tmp.y >= 2 && tmp.x < tmp.y) { // BOTH POS
             tmp.x -= 1;
             tmp.y -= 2;
             count++;
             console.log("2nd " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x <= -1 && tmp.y <= -2 && tmp.x > tmp.y) { // BOTH NEG
             tmp.x += 1;
             tmp.y += 2;
             count++;
             console.log("3rd " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x <= -2 && tmp.y <= -1 && tmp.x < tmp.y) { // BOTH NEG
             tmp.x += 2;
             tmp.y += 1;
             count++;
             console.log("4th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x <= -1 && tmp.y >= 2 && tmp.x < tmp.y) { //X is NEG
             tmp.x += 1;
             tmp.y -= 2;
             count++;
             console.log("5th " + count);
-
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x <= -2 && tmp.y >= 1 && tmp.x < tmp.y) { //X is NEG
             tmp.x += 2;
             tmp.y -= 1;
             count++;
             console.log("6th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
         } else if (tmp.x >= 1 && tmp.y <= -2 && tmp.x > tmp.y) { //Y is NEG
             tmp.x -= 1;
             tmp.y += 2;
             count++;
             console.log("7th " + count);
-        } else if (tmp.x >= 2 && tmp.y >= -1 && tmp.x > tmp.y) { //Y is NEG
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x >= 2 && tmp.y <= -1 && tmp.x > tmp.y) { //Y is NEG
             tmp.x -= 2;
             tmp.y += 1;
             count++;
             console.log("8th " + count);
-        } else if (tmp.x === tmp.y && tmp.x !== 0) {
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x === 0 && tmp.y >= 2) {
+            tmp.x += 1;
+            tmp.y -= 2;
+            count++;
+            console.log("9th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x === 0 && tmp.y >= 1) {
+            tmp.x += 2;
+            tmp.y -= 1;
+            count++;
+            console.log("10th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x === 0 && tmp.y <= -2) {
+            tmp.x -= 1;
+            tmp.y += 2;
+            count++;
+            console.log("11th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x === 0 && tmp.y <= -1) {
+            tmp.x -= 2;
+            tmp.y += 1;
+            count++;
+            console.log("12th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x >= 2 && tmp.y === 0) {
+            tmp.x -= 2;
+            tmp.y += 1;
+            count++;
+            console.log("13th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x >= 1 && tmp.y === 0) {
+            tmp.x -= 1;
+            tmp.y += 2;
+            count++;
+            console.log("14th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x <= -2 && tmp.y === 0) {
+            tmp.x += 2;
+            tmp.y -= 1;
+            count++;
+            console.log("15th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if (tmp.x <= -1 && tmp.y === 0) {
+            tmp.x -= 1;
+            tmp.y -= 2;
+            count++;
+            console.log("16th " + count);
+            console.log("Values :" + tmp.x + "," + tmp.y);
+        } else if ((tmp.x === tmp.y) && (tmp.x !== 0) && (tmp.y !== 0)) {
             tmp.x -= 2;
             tmp.y -= 1;
             count++;
             console.log("end " + count);
+        } else if (tmp.x === 0 && tmp.y === 0) {
+            i = 0;
         }
-    }
-    while (tmp.x !== 0 && tmp !== 0);
+    } while (i === 1);//(tmp.x !== 0) && (tmp.y !== 0));//
     alert("Solution: " + count);
+    console.log("Solution: " + count);
 }
